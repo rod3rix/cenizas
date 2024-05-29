f2e48af1ef3849965f1834d932fdf023a0ed0abc
Rodrigo <rod@zimple.pro>
2024-05-29 17:48:46 -0400

    Update login.blade.php
    agregado logo
    agregada clase en links

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,14 +6,15 @@
     <div class="row justify-content-center">
         <div class="col-md-4">
             <div class="card">
-                <div class="card-header">{{ __('GRUPO MINERO LAS CENIZAS') }}</div>
-
+                <div class="card-header logo">
+                    <img src="https://comuni.zlab.cl/assets/images/logo-cenizas-color.png" alt="GRUPO MINERO LAS CENIZAS">
+                </div>
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="row">
-                            <label for="email" class="col-md-4 col-form-label text-align-left">{{ __(' Usuario') }}</label>
+                        <div class="row mb-2">
+                            <label for="email" class="col-12 col-form-label text-align-left">{{ __(' Usuario') }}</label>
 
                             <div class="col-md-12">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -26,8 +27,8 @@
                             </div>
                         </div>
 
-                        <div class="row mb-1">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Contraseña') }}</label>
+                        <div class="row mb-3">
+                            <label for="password" class="col-12 col-form-label text-align-left">{{ __('Contraseña') }}</label>
 
                             <div class="col-md-12">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -68,17 +69,15 @@
                     </form>
                     </div>
                     </div>
-                        <br>
-                        <br>
-                        <div class="row mb-0">
+                        <div class="row mt-4 mb-2">
                             <div class="col-md-12">
-                                    <a href="https://www.cenizas.cl/">{{ __('www.cenizas.cl') }}
+                                    <a class="linkcorp" href="https://www.cenizas.cl/">{{ __('www.cenizas.cl') }}
                                     </a>
                             </div>
                         </div>
                         <div class="row mb-0">
                             <div class="col-md-12">
-                                    <a href="{{ route('terminoCondiciones') }}">{{ __('Términos y condiciones') }}
+                                    <a class="linktc" href="{{ route('terminoCondiciones') }}">{{ __('Términos y condiciones') }}
                                     </a>
                             </div>
                         </div>
