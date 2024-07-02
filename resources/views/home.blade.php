65a29f809b08b5100dbbe275fb3d66531a39ce1c
Rodrigo <rod@zimple.pro>
2024-07-02 14:25:29 -0400

    cajas responsive

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,9 +6,9 @@
           <p class="mb-3">Acceda al <b>Portal Fondos</b> de Grupo Minera Las Cenizas. Aquí podrá informarse de los fondos concursables con los que contamos, postular tus proyectos y realizar sugerencias o reclamos.</p>
         <div class="card-deck mb-3 text-center">
         <div class="row mb-3 text-center">
-      <div class="col-4 themed-grid-col"><div class="card fondos mb-4 shadow-sm">
+      <div class="col-md-4 themed-grid-col"><div class="card fondos mb-4 shadow-sm">
           <div class="card-header">
-               <h4>FONDOS CONCURSABLES</h4>
+               <h4>Fondos Concursables</h4>
           </div>
           <div class="card-body">
             <a href="{{ route('postularFondos') }}" class="btn btn-lg btn-block btn-primary">Postular Fondos Concursables</a>
@@ -16,7 +16,7 @@
             <a href="{{ route('seguimientoFondos') }}" class="btn btn-lg btn-block btn-primary">Ver Estado Postulaciones</a>
           </div>
         </div></div>
-      <div class="col-4 themed-grid-col"><div class="card proyectos mb-4 shadow-sm">
+      <div class="col-md-4 themed-grid-col"><div class="card proyectos mb-4 shadow-sm">
           <div class="card-header">
                 <h4>Apoyo para proyectos</h4>
           </div>
@@ -26,9 +26,9 @@
             <a href="{{ route('seguimientoProyectos') }}" class="btn btn-lg btn-block btn-primary">Ver Estado Postulaciones</a>
           </div>
         </div></div>
-      <div class="col-4 themed-grid-col">  <div class="card sugerencias mb-4 shadow-sm">
+      <div class="col-md-4 themed-grid-col">  <div class="card sugerencias mb-4 shadow-sm">
           <div class="card-header">
-            <h4>sugerencias/RECLAMO</h4>
+            <h4>sugerencias/Reclamo</h4>
           </div>
           <div class="card-body">   
             <a href="{{ route('enviarCaso') }}" class="btn btn-lg btn-block btn-primary">Ingresar Casos</a>
