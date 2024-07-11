771199773f512f6233554312149fdd03718fcffc
Rodrigo <rod@zimple.pro>
2024-07-11 16:50:08 -0400

    Update postularProyectos.blade.php
    Cambio en titulo y texto bajada

--- resources/views/postularProyectos.blade.php
+++ resources/views/postularProyectos.blade.php
@@ -1,15 +1,17 @@
 @extends('layouts.app')
 @section('content')
 <div class="jumbotron">
-        <div class="container">
-          <h3><b>POSTULACIÓN APOYO PROYECTO</b></h3>
-          <p>Grupo Minera Las Cenizas se encuentra comprometido con el desarrollo social y económico de las comunidades en las cuales se encuentran sus proyectos. En función de lo anterior, dispone de financiamiento para aportar a dicho crecimiento y desarrollo. Por este mecanismo puedes postular tu proyecto el cual se puede enmarcar en las siguientes temáticas: cultura y patrimonio, deporte y recreación, desarrollo económico, educación, infraestructura y equipamiento comunitario, medioambiente, salud, voluntariado, animalismo, inclusión u otra temática relevante.</p>
-        </div>
-        <hr>
+  <div class="container-fluid headpage">
+    <div class="row justify-content-center headinner">
+      <h1>Postulación apoyo proyecto</h1>
+      <p>Grupo Minera Las Cenizas se encuentra comprometido con el desarrollo social y económico de las comunidades en las cuales se encuentran sus proyectos. En función de lo anterior, dispone de financiamiento para aportar a dicho crecimiento y desarrollo. Por este mecanismo puedes postular tu proyecto el cual se puede enmarcar en las siguientes temáticas: cultura y patrimonio, deporte y recreación, desarrollo económico, educación, infraestructura y equipamiento comunitario, medioambiente, salud, voluntariado, animalismo, inclusión u otra temática relevante.</p>
+    </div>
+  </div>
+        
     <div class="container text-center">
-          <button type="button" id="bt_et1" class="btn btn-info btn-arrow-right btn-lg">ANTECEDENTES GENERALES</button>
-          <button type="button" id="bt_et2" class="btn btn-arrow-right btn-lg">DATOS ORGANIZACIÓN/MIPYME</button>
-          <button type="button" id="bt_et3"class="btn  btn-arrow-right btn-lg">POSTULACIÓN</button>
+          <button type="button" id="bt_et1" class="btn btn-info btn-arrow-right btn-lg">Antecedentes generales</button>
+          <button type="button" id="bt_et2" class="btn btn-arrow-right btn-lg">Datos organización / MIPYME</button>
+          <button type="button" id="bt_et3"class="btn  btn-arrow-right btn-lg">Postulación</button>
     </div>
     <div class="container">
     <form method="POST" id="frm_proy" name="frm_proy" enctype="multipart/form-data">
